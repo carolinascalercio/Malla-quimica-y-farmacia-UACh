0f84faf88a8a69773ee1cbbc1c2eaf107ccda0ea
carolinascalercio <carolinascalercio@icloud.com>
2026-01-15 23:37:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Cambia entre pendiente y aprobado cuando se hace clic en el ramo
+function marcarRamo(ramoId) {
+    let ramo = document.getElementById(ramoId);
+
+    if (ramo.classList.contains('pendiente')) {
+        ramo.classList.remove('pendiente');
+        ramo.classList.add('aprobado');
+    } else if (ramo.classList.contains('aprobado')) {
+        ramo.classList.remove('aprobado');
+        ramo.classList.add('pendiente');
+    }
+}
+
+// Bloquea los ramos que dependen de otros, cambiándolos a gris
+function bloquearRamo(ramoId) {
+    let ramo = document.getElementById(ramoId);
+    ramo.classList.add('preRequisito');
+    alert("Este ramo está bloqueado, necesitas aprobar el ramo anterior primero.");
+}
 
